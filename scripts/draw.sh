67aa3913cf7c7df1e09c48b19428a09681029210
Afonso <afonso.tfaria21@gmail.com>
2025-03-29 15:47:07 -0400

    Improve figures

--- scripts/draw.sh
+++ scripts/draw.sh
@@ -1,12 +1,22 @@
 set -e
 files="./results/olabs_oram_1742692087_bbab52166942eecb604ca86c710b5ecbe592ba26 ./results/olabs_rods_1742690948_4213ad1dce057d9249ad09f9220a90f7271e5b2b ./results/signal_icelake_1742700453_ca00ab6b9eb5152277473a2755110b176975f4a4 ./results/meta_oram_1743207872_b0bffaeca5a3b7f2522df1e68ad87e15a9a2d3e9"
 
-python ./scripts/draw_figure.py -f $files  -x N -y Read_latency_us -l '{implementation}' -c 'Key_bytes=8' 'Value_bytes=8' 'benchmark_type=RORAM' -t 'RORAM' 
-python ./scripts/draw_figure.py -f $files  -x N -y Initialization_zeroed_time_us -l '{implementation}' -c 'Key_bytes=8' 'Value_bytes=8' 'benchmark_type=RORAM' -t 'RORAM' 
-python ./scripts/draw_figure.py -f $files  -x N -y Read_throughput_qps -l '{implementation}' -c 'Key_bytes=8' 'Value_bytes=8' 'benchmark_type=RORAM' -t 'RORAM' 
-python ./scripts/draw_figure.py -f $files  -x N -y Memory_kb -l '{implementation}' -c 'Key_bytes=8' 'Value_bytes=8' 'benchmark_type=RORAM' -t 'RORAM' 
+python ./scripts/draw_figure.py -f $files  -x N -y Read_latency_us -yl '$\mu s$' -l '{implementation}' -c 'Key_bytes=8' 'Value_bytes=8' 'benchmark_type=RORAM' -t 'RORAM Read Latency' 
+python ./scripts/draw_figure.py -f $files  -x N -y Initialization_zeroed_time_us -yl '$\mu s$' -l '{implementation}' -c 'Key_bytes=8' 'Value_bytes=8' 'benchmark_type=RORAM' -t 'RORAM Initialization Zeroed Time' 
+python ./scripts/draw_figure.py -f $files  -x N -y Read_throughput_qps -yl 'queries/s' -l '{implementation}' -c 'Key_bytes=8' 'Value_bytes=8' 'benchmark_type=RORAM' -t 'RORAM Read Throughput' 
+python ./scripts/draw_figure.py -f $files  -x N -y Memory_kb -yl 'Memory (kb)' -l '{implementation}' -c 'Key_bytes=8' 'Value_bytes=8' 'benchmark_type=RORAM' -t 'RORAM Memory' 
 
-python ./scripts/draw_figure.py -f $files  -x N -y Read_latency_us -l '{implementation}' -c 'Key_bytes=8' 'Value_bytes=8' 'benchmark_type=NRORAM' -t 'NRORAM' 
-python ./scripts/draw_figure.py -f $files  -x N -y Initialization_zeroed_time_us -l '{implementation}' -c 'Key_bytes=8' 'Value_bytes=8' 'benchmark_type=NRORAM' -t 'NRORAM' 
-python ./scripts/draw_figure.py -f $files  -x N -y Read_throughput_qps -l '{implementation}' -c 'Key_bytes=8' 'Value_bytes=8' 'benchmark_type=NRORAM' -t 'NRORAM' 
-python ./scripts/draw_figure.py -f $files  -x N -y Memory_kb -l '{implementation}' -c 'Key_bytes=8' 'Value_bytes=8' 'benchmark_type=NRORAM' -t 'NRORAM' 
+python ./scripts/draw_figure.py -f $files  -x N -y Read_latency_us -yl '$\mu s$' -l '{implementation}' -c 'Key_bytes=8' 'Value_bytes=8' 'benchmark_type=NRORAM' -t 'NRORAM Read Latency' 
+python ./scripts/draw_figure.py -f $files  -x N -y Initialization_zeroed_time_us -yl '$\mu s$' -l '{implementation}' -c 'Key_bytes=8' 'Value_bytes=8' 'benchmark_type=NRORAM' -t 'NRORAM Initialization Zeroed Time' 
+python ./scripts/draw_figure.py -f $files  -x N -y Read_throughput_qps -yl 'queries/s' -l '{implementation}' -c 'Key_bytes=8' 'Value_bytes=8' 'benchmark_type=NRORAM' -t 'NRORAM Read Throughput' 
+python ./scripts/draw_figure.py -f $files  -x N -y Memory_kb -yl 'Memory (kb)' -l '{implementation}' -c 'Key_bytes=8' 'Value_bytes=8' 'benchmark_type=NRORAM' -t 'NRORAM Memory' 
+
+python ./scripts/draw_figure.py -f $files  -x N -y Get_latency_us -yl '$\mu s$' -l '{implementation}' -c 'Key_bytes=8' 'Value_bytes=56' 'benchmark_type=UnorderedMap' '!Shards' -t 'OMap Get Latency' 
+python ./scripts/draw_figure.py -f $files  -x N -y Initialization_time_us -yl '$\mu s$' -l '{implementation}' -c 'Key_bytes=8' 'Value_bytes=56' 'benchmark_type=UnorderedMap' '!Shards' -t 'OMap Initialization Time' 
+python ./scripts/draw_figure.py -f $files  -x N -y Get_throughput_qps -yl 'queries/s' -l '{implementation}' -c 'Key_bytes=8' 'Value_bytes=56' 'benchmark_type=UnorderedMap' '!Shards' -t 'OMap Throughput' 
+python ./scripts/draw_figure.py -f $files  -x N -y Memory_kb -yl 'Memory (kb)' -l '{implementation}' -c 'Key_bytes=8' 'Value_bytes=56' 'benchmark_type=UnorderedMap' '!Shards' -t 'OMap Memory' 
+
+python ./scripts/draw_figure.py -f $files  -x N -y Get_latency_us -yl '$\mu s$' -l '{implementation}-{Batch_size}' -c 'Key_bytes=8' 'Value_bytes=56' 'benchmark_type=UnorderedMap' 'Batch_size@100,1000,4096' -t 'OMap_Sharded Get Latency'   
+python ./scripts/draw_figure.py -f $files  -x N -y Initialization_time_us -yl '$\mu s$' -l '{implementation}-{Batch_size}' -c 'Key_bytes=8' 'Value_bytes=56' 'benchmark_type=UnorderedMap' 'Batch_size@100,1000,4096' -t 'OMap_Sharded Initialization Time' 
+python ./scripts/draw_figure.py -f $files  -x N -y Get_throughput_qps -yl 'queries/s' -l '{implementation}-{Batch_size}' -c 'Key_bytes=8' 'Value_bytes=56' 'benchmark_type=UnorderedMap' 'Batch_size@100,1000,4096' -t 'OMap_Sharded Throughput' 
+python ./scripts/draw_figure.py -f $files  -x N -y Memory_kb -yl 'Memory (kb)' -l '{implementation}-{Batch_size}' -c 'Key_bytes=8' 'Value_bytes=56' 'benchmark_type=UnorderedMap' 'Batch_size@100,1000,4096' -t 'OMap_Sharded Memory' 
--- scripts/draw_figure.py
+++ scripts/draw_figure.py
@@ -18,7 +18,15 @@ ap.add_argument("-c", "--constraints", type=str, nargs="+", default=[], help="Co
 ap.add_argument("-o", "--output", type=str, default="figures/", help="Output file directory.") 
 # title
 ap.add_argument("-t", "--title", type=str, default="Plot", help="Title of the plot.")
+# x axis label
+ap.add_argument("-xl", "--xlabel", type=str, help="X axis label.")
+# y axis label  
+ap.add_argument("-yl", "--ylabel", type=str, help="Y axis label.")
 
+class DefaultDict(dict):
+  def __missing__(self, key):
+    # Set default values here
+    return ""
 
 args = ap.parse_args()
 
@@ -30,14 +38,53 @@ for file in args.files:
 
 constraints = []
 for constraint in args.constraints:
-  key, value = constraint.split("=")
-  constraints.append((key, value))
+  if "!=" in constraint:
+    key, value = constraint.split("!=")
+    constraints.append(("!=", key, value))
+  elif "=" in constraint:
+    key, value = constraint.split("=")  
+    constraints.append(("=", key, value))
+  elif constraint.startswith("!"):
+    key = constraint[1:]
+    value = ""
+    constraints.append(("!", key, value))
+  elif "@" in constraint:
+    key, values = constraint.split("@")
+    values = values.split(",")
+    constraints.append(("@", key, values))
+  else:
+    key = constraint
+    value = ""
+    constraints.append(("", key, value))
 
 pts = defaultdict(list)
 for line in lines:
-  pt = json.loads(line)
-  for key, value in constraints:
-    if key not in pt or pt[key] != value:
+  try:
+    pt = json.loads(line)
+  except json.JSONDecodeError as e:
+    print(f"Error decoding JSON: {e} (line: {line})")
+    continue
+  for op, key, value in constraints:
+    add = True
+    if op in ["!=", "=", "", "@"]:
+      if key not in pt: add = False
+    elif op == "!":
+      if key in pt: add = False
+    else:
+      assert False, f"Unknown operator {op}"
+
+    if add:  
+      if op == "=":
+        if pt[key] != value:
+          add = False
+      elif op == "!=":    
+        if pt[key] == value:
+          add = False
+      elif op == "@":
+        if pt[key] not in value:
+          add = False
+
+    if not add:
       print(f"Skipping {pt} because {key} != {value}")
       break
   else:
@@ -45,14 +92,37 @@ for line in lines:
     try:
       x = pt[args.xparam]
       y = pt[args.yparam]
-      name = args.linenameparam.format_map(pt)
+      name = args.linenameparam.format_map(DefaultDict(pt))
       x = float(x)
       y = float(y)
       pts[name].append((x, y))
-    except KeyError:
+    except Exception as e:
+      print(f"Exception: {e}")
       continue
 
-for name, points in pts.items():
+# Build a mapping from line name to dict of x -> y for legend sorting
+name_to_xy = {name: dict(points) for name, points in pts.items()}
+
+# Find common x values across all line groups
+all_x_sets = [set(xy.keys()) for xy in name_to_xy.values()]
+if all_x_sets:
+    common_xs = set.intersection(*all_x_sets)
+else:
+    common_xs = set()
+
+if not common_xs:
+    print("Warning: No common x values between lines. Using unsorted legend.")
+    sorted_names = list(pts.keys())
+else:
+    # Use the largest common x for sorting
+    x_common = max(common_xs)
+    sorted_names = sorted(name_to_xy.keys(), key=lambda name: name_to_xy[name][x_common], reverse=True)
+
+
+
+
+for name in sorted_names:
+  points = pts[name]
   print(points)
   points = sorted(points)
   x_values = [point[0] for point in points]
@@ -61,13 +131,20 @@ for name, points in pts.items():
   xs, ys = zip(*points)
   plt.plot(xs, ys, label=name)
 
-plt.xlabel(args.xparam)
-plt.ylabel(args.yparam)
+if args.xlabel:
+  plt.xlabel(args.xlabel)
+else:
+  plt.xlabel(args.xparam) 
+if args.ylabel:
+  plt.ylabel(args.ylabel) 
+else:  
+  plt.ylabel(args.yparam)
 plt.title(f"{args.title}")
 plt.legend()
 plt.grid(True)
-plt.tight_layout()
+# plt.tight_layout()
 plt.xscale("log")
 plt.yscale("log")
-plt.savefig(f"{args.output}/{args.title}_{args.yparam}.png")
+filename = f'{args.title.replace(" ", "_").replace(":", "-").replace("/", "-")}'
+plt.savefig(f"{args.output}/{filename}.png")
 
