7590183b2c1b97cd34ae65e4fc13201e0e19a526
Afonso Tinoco <afonso.tfaria21@gmail.com>
2025-04-08 10:34:30 -0400

    Adds setup description

--- go.sh
+++ go.sh
@@ -2,6 +2,12 @@
 set -e
 sh ./scripts/setup.sh
 
+### Checklist before this:
+# 1) Make sure you have installed the msrp (rust)
+# 2) Make sure you have run the setup tools script in olabs_rods
+# 3) Make sure you have installed all the c++ packages required for signal_icelake and olabs_rods
+# 4) Make sure you have docker running
+
 sh ./benchmark/meta_oram/run.sh
 sh ./benchmark/olabs_rods/run.sh
 sh ./benchmark/olabs_oram/run.sh
