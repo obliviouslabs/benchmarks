d550eacaa3034779c0c1f47fe94bdb54fc677d26
Afonso <afonso.tfaria21@gmail.com>
2025-07-30 20:01:36 -0400

    Adds 8b8b benchmark to rostl umap

--- benchmark/olabs_rostl/benchmark_code/src/main.rs
+++ benchmark/olabs_rostl/benchmark_code/src/main.rs
@@ -105,19 +105,19 @@ impl_cmov_for_pod!(B448);
 
 
 
-fn benchmark_umap(n: usize) -> i32 {
+fn benchmark_umap<VT: Default + Ord + Cmov + Pod + std::fmt::Debug + Send>(n: usize) -> i32 {
   let mem_start = get_mem_value().unwrap_or(0);
   let start_create_ns = current_time_ns();
   let mut rng = rand::rng();
   const READ_SAMPLES :u64 = 500_000;
 
-  let mut umap = UnsortedMap::<u64, B448>::new(n);
-  umap.insert(0, B448 { a: 0, b: 0, c: 0, d: 0, e: 0, f: 0, g: 0 });
+  let mut umap = UnsortedMap::<u64, VT>::new(n);
+  umap.insert(0, VT::default());
   let end_create_ns = current_time_ns();
 
   let start_query_ns = current_time_ns();
   for _ in 0..READ_SAMPLES {
-    let mut _value = B448 { a: 0, b: 0, c: 0, d: 0, e: 0, f: 0, g: 0 };
+    let mut _value = VT::default();
     black_box(umap.get(black_box(0), black_box(&mut _value)));
   }
   let end_query_ns = current_time_ns();
@@ -128,8 +128,9 @@ fn benchmark_umap(n: usize) -> i32 {
   report_line!(
     "UnorderedMap",
     "olabs_rostl",
-    "N := {} | Key_bytes := 8 | Value_bytes := 56 | fill := 0.8 | Initialization_zeroed_time_us := {} | Get_latency_us := {} | Get_throughput_qps := {} | Memory_kb := {}",
+    "N := {} | Key_bytes := 8 | Value_bytes := {} | fill := 0.8 | Initialization_zeroed_time_us := {} | Get_latency_us := {} | Get_throughput_qps := {} | Memory_kb := {}",
     n,
+    size_of::<VT>(),
     create_time_ns / 1_000,
     avg_ns / 1_000.0,
     (READ_SAMPLES as f64 / (end_query_ns - start_query_ns) as f64) * 1e9,
@@ -140,7 +141,7 @@ fn benchmark_umap(n: usize) -> i32 {
 }
 
 
-fn benchmark_sharded_umap<const B: usize, const queries_per_batch: usize>(n: usize) -> i32{
+fn benchmark_sharded_umap<VT: Default + Ord + Cmov + Pod + std::fmt::Debug + Send, const B: usize, const queries_per_batch: usize>(n: usize) -> i32{
   const READ_SAMPLES :usize = 1_000_000;
 
   let mem_start = get_mem_value().unwrap_or(0);
@@ -149,7 +150,7 @@ fn benchmark_sharded_umap<const B: usize, const queries_per_batch: usize>(n: usi
   let num_batches = READ_SAMPLES.div_ceil(queries_per_batch) as usize;
   let num_queries = num_batches * queries_per_batch;
 
-  let mut umap = ShardedMap::<u64, B448, B>::new(n);
+  let mut umap = ShardedMap::<u64, VT, B>::new(n);
   let end_create_ns = current_time_ns();
 
   let start_query_ns = current_time_ns();
@@ -174,8 +175,9 @@ fn benchmark_sharded_umap<const B: usize, const queries_per_batch: usize>(n: usi
   report_line!(
     "UnorderedMap",
     "olabs_rostl_sharded",
-    "N := {} | Key_bytes := 8 | Value_bytes := 56 | fill := 0.8 | Batch_size := {} | Shards := 15 | Initialization_zeroed_time_us := {} | Get_latency_us := {} | Get_throughput_qps := {} | Memory_kb := {}",
+    "N := {} | Key_bytes := 8 | Value_bytes := {} | fill := 0.8 | Batch_size := {} | Shards := 15 | Initialization_zeroed_time_us := {} | Get_latency_us := {} | Get_throughput_qps := {} | Memory_kb := {}",
     n,
+    size_of::<VT>(),
     queries_per_batch,
     create_time_ns / 1_000,
     avg_latency_ns / 1_000.0,
@@ -213,7 +215,15 @@ fn main() {
     let val = 1 << i;
     let test_name = format!("benchmark_umap<u64,B448>(1<<{})", i);
     run_test_forked(&test_name, || {
-      benchmark_umap(val)
+      benchmark_umap::<B448>(val)
+    });
+  }
+
+  for i in 10..=26 {
+    let val = 1 << i;
+    let test_name = format!("benchmark_umap<u64,u64>(1<<{})", i);
+    run_test_forked(&test_name, || {
+      benchmark_umap::<u64>(val)
     });
   }
 
@@ -222,7 +232,7 @@ fn main() {
     let val = 1 << i;
     let test_name = format!("benchmark_sharded_umap<u64,B448>(1<<{})", i);
     run_test_forked(&test_name, || {
-      benchmark_sharded_umap::<305, 4096>(val)
+      benchmark_sharded_umap::<B448, 305, 4096>(val)
     });
   }
 
